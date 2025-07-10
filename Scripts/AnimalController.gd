50a28632446141eb4a92d717e20b078845268f55
Noah Turner <noah.j.turner@outlook.com>
2025-07-10 18:29:18 -0500

    implemented finite state machine for basic movement

--- Scripts/AnimalController.gd
+++ Scripts/AnimalController.gd
@@ -1,45 +1,49 @@
 extends CharacterBody3D
 
+@export var walk_speed: float = 1.0
+@export var run_speed: float = 2.0
+@export var player: CharacterBody3D
+@onready var animal: Node3D = $deer
+@onready var hitbox: CollisionShape3D = $Hitbox
 
-
-var movement_speed: float = 2.0
-var movement_target_position: Vector3 = Vector3(-3.0,0.0,2.0)
-@onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
-var hitbox
 
 func _ready() -> void:
-	hitbox = $Hitbox as CollisionShape3D
-	navigation_agent.path_desired_distance = 0.5
-	navigation_agent.target_desired_distance = 0.5
-
-	# Make sure to not await during _ready.
-	actor_setup.call_deferred()
-
+	pass
+	
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	
+	#Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		
-	if navigation_agent.is_navigation_finished():
-		return
-
-	var current_agent_position: Vector3 = global_position
-	var next_path_position: Vector3 = navigation_agent.get_next_path_position()
-
-	velocity = current_agent_position.direction_to(next_path_position) * movement_speed
 
 	if(!hitbox.disabled):
+		#IMPORTANT: set the rotation before aligning with surface normal
+		look_at(global_position + velocity, Vector3.UP, true)
+		align_with_surface()
+		
 		move_and_slide()
 	else:
-		pass
+		return
 
+func align_with_surface():
+	if(!$SurfaceNormal.is_colliding()):
+		return
+		
+	var normal = $SurfaceNormal.get_collision_normal() as Vector3
+	var basis_new = Basis()
+	var scale_base = basis.get_scale()
 
-func actor_setup():
-	# Wait for the first physics frame so the NavigationServer can sync.
-	await get_tree().physics_frame
-
-	# Now that the navigation map is no longer empty, set the movement target.
-	set_movement_target(movement_target_position)
-
-func set_movement_target(movement_target: Vector3):
-	navigation_agent.set_target_position(movement_target)
+	#Linear algebra bullshit
+	basis_new.x = normal.cross(global_basis.z)
+	basis_new.y = normal
+	basis_new.z = global_basis.x.cross(normal)
+	
+	basis_new = basis_new.orthonormalized()
+	
+	basis_new.x *= scale_base.x 
+	basis_new.y *= scale_base.y
+	basis_new.z *= scale_base.z 
+	
+	global_basis = basis_new
+	
+	
---
+++ Scripts/AnimalStates.gd
@@ -0,0 +1,63 @@
+extends Node3D
+
+enum State{IDLE, WALK, FLEE}
+var state 
+
+@export var animal: CharacterBody3D
+@onready var navigation_agent: NavigationAgent3D = get_node("../NavigationAgent3D")
+
+
+func _ready() -> void:
+	change_state(State.WALK)
+	
+	navigation_agent.path_desired_distance = 0.5
+	navigation_agent.target_desired_distance = 0.5
+	
+	actor_setup.call_deferred()
+
+func _physics_process(delta: float) -> void:
+	#print("Poo")
+	
+	match state:
+		State.IDLE: _idle_state(delta)
+		State.WALK: _walk_state(delta)
+		State.FLEE: _flee_state(delta)
+	
+	if navigation_agent.is_navigation_finished():
+		change_state(State.IDLE)
+		return
+		
+
+
+func change_state(s):
+	if(state == s):
+		return
+	if(s == State.WALK):
+		var movement_target_position = Vector3(global_position.x + randf_range(-20.0, 10.0), global_position.y, global_position.z + randf_range(-20.0, 10.0))
+		set_movement_target(movement_target_position)
+	state = s
+
+func _idle_state(delta):
+	animal.velocity = Vector3.ZERO
+
+	
+func _walk_state(delta):
+
+	
+	var current_animal_position: Vector3 = animal.global_position
+	var next_path_position: Vector3 = navigation_agent.get_next_path_position()
+
+	animal.velocity = current_animal_position.direction_to(next_path_position) * animal.walk_speed
+	
+func _flee_state(delta):
+	pass
+	
+	
+func actor_setup():
+	# Wait for the first physics frame so the NavigationServer can sync.
+	await get_tree().physics_frame
+
+
+func set_movement_target(movement_target: Vector3):
+	navigation_agent.set_target_position(movement_target)
+	
