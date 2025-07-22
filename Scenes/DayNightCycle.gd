3770e457f031fa2b797baf71428900eacd70acbb
Noah Turner <noah.j.turner@outlook.com>
2025-07-22 16:33:49 -0500

    added clouds and stars

--- Scenes/DayNightCycle.gd
+++ Scenes/DayNightCycle.gd
@@ -3,7 +3,7 @@ extends WorldEnvironment
 
 @export var grad : Gradient
 @export_range(0.0, 2400.0, 0.1) var time_of_day : float = 1200.0
-@export_range(0.0, 360.0, 0.1) var cycle_rotation : float = -90.0
+@export_range(-90.0, 90.0, 0.1) var cycle_rotation : float = -90.0
 @onready var sun_moon : Node3D = $SunMoon
 @onready var sun : MeshInstance3D = $SunMoon/Sun
 @onready var moon : MeshInstance3D = $SunMoon/Moon
@@ -22,4 +22,7 @@ func _process(delta: float) -> void:
 	var sky_material : ShaderMaterial = self.environment.sky.sky_material
 	sun_moon.rotation_degrees.x = cycle_rotation
 	sky_material.set_shader_parameter("baseColor", grad.sample(sun_pos))
+	var col : Color = sky_material.get_shader_parameter("cloudColor")
+	col.a = sun_pos
+	sky_material.set_shader_parameter("cloudColor", col)
 	sun_light.light_energy = sun_pos
