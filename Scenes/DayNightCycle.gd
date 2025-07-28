039f697c46c20cdfe5b48df62860c5c0ef17f2d5
Noah Turner <noah.j.turner@outlook.com>
2025-07-28 14:49:30 -0500

    finished day night cycle

--- Scenes/DayNightCycle.gd
+++ Scenes/DayNightCycle.gd
@@ -1,11 +1,10 @@
-@tool
 extends WorldEnvironment
 
 @export var light : float = 1.0
 @export var sky_grad : Gradient
 @export var sky_light_grad : Gradient
 @export var day_cycle_speed : float = 1.0
-@export_range(-85.0, 85.0, 0.1) var cycle_rotation : float = -85.0
+@export_range(-100.0, 100.0, 0.1) var cycle_rotation : float = -100.0
 @onready var sun_moon : Node3D = $SunMoon
 @onready var sun : MeshInstance3D = $SunMoon/Sun
 @onready var moon : MeshInstance3D = $SunMoon/Moon
@@ -29,7 +28,6 @@ func _process(delta: float) -> void:
 	sky_material.set_shader_parameter("sunPosy", sun.global_position.y)
 	sky_material.set_shader_parameter("skyColor", sky_light_grad.sample(sun_pos_y))
 	
-	sun_light.light_energy = 0.0 if sun_pos_y <= 0.52 else (sin(sun_pos_y) * light)
-	#if(cycle_rotation < 80.0):
-		#print(sun_pos_y)
-		#cycle_rotation += day_cycle_speed * delta
+	sun_light.light_energy =  pow(sin(sun_pos_y) * light, 4.0)
+	if(cycle_rotation < 100.0):
+		cycle_rotation += day_cycle_speed * delta
